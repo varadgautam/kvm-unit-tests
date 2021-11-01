f45b3665bda7040b8cb7e9f1dc1eaacaa4eae8fb
Varad Gautam <varad.gautam@suse.com>
2021-11-01 14:59:09 +0100

    lib: x86: Import insn decoder from Linux
    Processing #VC exceptions on AMD SEV-ES requires instruction decoding
    logic to set up the right GHCB state before exiting to the host.
    
    Pull in the instruction decoder from Linux for this purpose.
    
    Origin: Linux 64222515138e43da1fcf288f0289ef1020427b87
    
    Signed-off-by: Varad Gautam <varad.gautam@suse.com>

---
+++ lib/x86/insn/inat_types.h
@@ -0,0 +1,18 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+#ifndef _ASM_X86_INAT_TYPES_H
+#define _ASM_X86_INAT_TYPES_H
+/*
+ * x86 instruction attributes
+ *
+ * Written by Masami Hiramatsu <mhiramat@redhat.com>
+ *
+ * Origin: Linux 64222515138e43da1fcf288f0289ef1020427b87
+ *   tools/arch/x86/include/asm/inat_types.h
+ */
+
+/* Instruction attributes */
+typedef unsigned int insn_attr_t;
+typedef unsigned char insn_byte_t;
+typedef signed int insn_value_t;
+
+#endif
