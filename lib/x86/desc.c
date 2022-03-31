f938933f54c6d09cba61db24e0334a813aee8ad2
Varad Gautam <varad.gautam@suse.com>
2022-03-31 13:29:38 +0200

    x86: Move load_idt() to desc.c
    This allows sharing IDT setup code between EFI (-fPIC) and
    non-EFI builds.
    
    Signed-off-by: Varad Gautam <varad.gautam@suse.com>

--- lib/x86/desc.c
+++ lib/x86/desc.c
@@ -293,6 +293,11 @@ void setup_idt(void)
     handle_exception(13, check_exception_table);
 }
 
+void load_idt(void)
+{
+	lidt(&idt_descr);
+}
+
 unsigned exception_vector(void)
 {
     unsigned char vector;
